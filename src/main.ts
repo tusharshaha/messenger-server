8de761284864900b559109b8a6ab52e2d658117f
tusharshaha <tusharshaha128@gmail.com>
2023-05-29 22:36:49 +0600

    removed prefix

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
   app.enableCors({ origin: process.env.FRONTEND_ORIGIN });
   await app.listen(5000);
 }
