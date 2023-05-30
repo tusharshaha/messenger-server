614d4056c176c3453aeaa9df535e1674b031b24c
tusharshaha <tusharshaha128@gmail.com>
2023-05-30 11:05:09 +0600

    updated server type

--- src/chat.gateway.ts
+++ src/chat.gateway.ts
@@ -4,11 +4,12 @@ import {
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { Server } from 'https';
+import { Server } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
 export class ChatGateWay {
   @WebSocketServer() server: Server;
+
   @SubscribeMessage('message')
   handleEvent(@MessageBody() message: string): void {
     this.server.emit('message', message);
