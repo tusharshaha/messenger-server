22254de365c104538189380046e9cd7b06a574f8
tusharshaha <tusharshaha128@gmail.com>
2023-05-30 12:50:40 +0600

    added onModule

--- src/chat.gateway.ts
+++ src/chat.gateway.ts
@@ -1,3 +1,4 @@
+import { OnModuleInit } from '@nestjs/common';
 import {
   MessageBody,
   SubscribeMessage,
@@ -6,9 +7,20 @@ import {
 } from '@nestjs/websockets';
 import { Server } from 'socket.io';
 
-@WebSocketGateway({ cors: true })
-export class ChatGateWay {
+@WebSocketGateway({
+  cors: {
+    origin: ['http://localhost:3000'],
+  },
+})
+export class ChatGateWay implements OnModuleInit {
   @WebSocketServer() server: Server;
+
+  onModuleInit() {
+    this.server.on('connection', (socket) => {
+      console.log(socket.id);
+      console.log('Connected');
+    });
+  }
 
   @SubscribeMessage('message')
   handleEvent(@MessageBody() message: string): void {
