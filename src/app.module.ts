e04cf48bd5c50fab78c5b2f39781e6594362dfb9
tusharshaha <tusharshaha128@gmail.com>
2023-07-13 22:16:18 +0600

    created user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ChatGateWay } from './chat.gateway';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -8,6 +9,7 @@ import { ChatGateWay } from './chat.gateway';
       envFilePath: '.env',
       isGlobal: true,
     }),
+    UserModule,
   ],
   providers: [ChatGateWay],
 })
