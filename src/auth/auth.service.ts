a4ccea3f53f02b8e1ac311e3881d11d070455205
tusharshaha <tusharshaha128@gmail.com>
2023-07-15 12:33:08 +0600

    creted signup service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,14 +2,24 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schema/user.schema';
 import { Model } from 'mongoose';
+import { signupDto } from './dto/signup.dto';
 
 @Injectable()
 export class AuthService {
   constructor(
-    @InjectModel(User.name) private readonly userModel: Model<User>
+    @InjectModel(User.name) private readonly userModel: Model<User>,
   ) {}
 
-  async signup(signupDto) {}
+  async signup(signupDto: signupDto) {
+    const { name, email, password } = signupDto;
+    const userBody = { name, email, password };
+    const user = new this.userModel(userBody);
+    const date = new Date();
+    // expire date is 30 days.
+    date.setDate(date.getDate() + 30);
+    user.userExpires = date;
+    await user.save({ validateBeforeSave: true });
 
+    return name;
+  }
 }
-
