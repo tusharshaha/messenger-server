8d72d2129404628fdfe7a21efc00002de46a7909
tusharshaha <tusharshaha128@gmail.com>
2023-07-15 11:36:54 +0600

    created auth resource and user schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ChatGateWay } from './chat.gateway';
-import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -9,7 +10,8 @@ import { UserModule } from './user/user.module';
       envFilePath: '.env',
       isGlobal: true,
     }),
-    UserModule,
+    AuthModule,
+    MongooseModule.forRoot(process.env.MONGO_URI),
   ],
   providers: [ChatGateWay],
 })
---
+++ src/auth/schema/user.schema.ts
@@ -0,0 +1,48 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+@Schema({
+  timestamps: true,
+})
+export class User extends Document {
+  @Prop({
+    required: [true, 'Please give your name'],
+    trim: true,
+    minLength: [3, 'Name must be at least 3 characters.'],
+    maxLength: [100, 'Name is too large'],
+  })
+  name: string;
+
+  @Prop({
+    unique: true,
+    required: [true, "Email can't be empty!"],
+    trim: true,
+    validate: {
+      validator: (value: string) => {
+        const regex = /^[a-zA-Z0-9._-]+@(?:gmail|yahoo|hotmail|outlook)\.com$/;
+        return regex.test(value);
+      },
+      message: 'Please provide a valid email',
+    },
+  })
+  email: string;
+
+  @Prop({
+    required: [true, "Password can't be empty!"],
+    validate: {
+      validator: (value: string) => {
+        const regex = /^(?=.*[a-z])(?=.*\d).{6,}$/;
+        return regex.test(value);
+      },
+      message: 'your password is not strong enough.',
+    },
+  })
+  password: string;
+
+  @Prop()
+  userExpires: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
+
+// create TTL index for invalid user
+UserSchema.index({ userExpires: 1 }, { expireAfterSeconds: 0 });
